b9b6c380e8f5a751ca10e32a4a0fc245e5750758
Vitriol-nT <161344449+Vitriol-nT@users.noreply.github.com>
2025-04-19 15:46:13 +0900

    experimental run code.py

--- BasicSimulation/experimental.py
+++ BasicSimulation/experimental.py
@@ -2,8 +2,10 @@ from BasicPhysics import bodies
 from BasicPhysics import plotting
 from BasicPhysics import Wall
 from BasicPhysics import ForTest
+import math
+#radian
 
 firstbody = bodies(Position=(0,0,100), Velocity=(0,2,0))
-thewall = Wall(firstbody, Position=(0,10,0), FaceAxis=(0,1,0))
+thewall = Wall(firstbody, Position=(0,10,0), Rotation=(0,1,0))
 p = ForTest(firstbody, Wall)
 p.update(25)
