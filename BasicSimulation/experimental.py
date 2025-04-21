0a916194a820dc6161d8ad7025e3f4928069135a
Vitriol-nT <161344449+Vitriol-nT@users.noreply.github.com>
2025-04-21 13:26:24 +0900

    RunCodel.py

--- BasicSimulation/experimental.py
+++ BasicSimulation/experimental.py
@@ -1,11 +1,8 @@
 from BasicPhysics import bodies
 from BasicPhysics import plotting
 from BasicPhysics import Wall
-from BasicPhysics import ForTest
-import math
-#radian
 
 firstbody = bodies(Position=(0,0,100), Velocity=(0,2,0))
-thewall = Wall(firstbody, Position=(0,10,0), Rotation=(0,1,0))
-p = ForTest(firstbody, Wall)
-p.update(25)
+thewall = Wall(firstbody, Position=(300,80,100), rotation=(0,1))
+visualize = plotting(firstbody, thewall)
+visualize.update(35)
