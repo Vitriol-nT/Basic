369a7b8ec1d5723023e62f2740e3c760cd734132
Vitriol-nT <161344449+Vitriol-nT@users.noreply.github.com>
2025-04-21 13:25:57 +0900

    BasicPhysics.py

--- BasicSimulation/BasicPhysics.py
+++ BasicSimulation/BasicPhysics.py
@@ -6,7 +6,7 @@ import math
 import tkinter.messagebox as tkmsg
 
 Gravity = -9.8
-Wind = 5
+Wind = 1
 col = False
 
 class bodies:
@@ -38,27 +38,30 @@ class Wall:
     self.rotation = list(rotation)
     self.body = body
 
-  def SetPlane(self):
-    def f(x, y):
-      z = math.tan(self.rotation[0]) * ( x - self.Position[0] ) + math.tan(self.rotation[1]) * ( y - self.Position[1]) + self.Position[2]
-      return z
+  def Plane(self, x, y):
+    z = math.tan(self.rotation[0]) * ( x - self.Position[0] ) + math.tan(self.rotation[1]) * ( y - self.Position[1]) + self.Position[2]
+    return z
 
   def VectorReflection(self):
+    global col
+
     Normal = (math.tan(self.rotation[0]), math.tan(self.rotation[1]), -1)
-    if self.body.Position[2] <= f(self.body.Position[0], self.body.Position[1]):
+    if self.body.Position[2] <= self.Plane(self.body.Position[0], self.body.Position[1]):
       col = True
 
     if col == True:
-      self.body.Position[2] = f(self.body.Position[0], self.body.Position[1])
+      self.body.Position[2] = self.Plane(self.body.Position[0], self.body.Position[1])
+      #reflection
+      self.body.Velocity[0] = self.body.Velocity[0] -2 * (self.body.Velocity[0] * Normal[0])
+      self.body.Velocity[1] = self.body.Velocity[1] -2 * (self.body.Velocity[1] * Normal[1])
+      self.body.Velocity[2] = self.body.Velocity[2] -2 * (self.body.Velocity[2] * Normal[2])
       col = False
-      self.body.Velocity[0] = self.body.Velocity[0] -2 * (self.body.Velocity[0]*Normal[0])
-      self.body.Velocity[1] = self.body.Velocity[1] -2 * (self.body.Velocity[1]*Normal[1])
-      self.body.Velocity[2] = self.body.Velocity[2] -2 * (self.body.Velocity[2]*Normal[2])
 
 class plotting:
-  def __init__(self, body):
+  def __init__(self, body, Wall):
     self.body = body
     self.history = [list(body.Position)]
+    self.Wall = Wall
 
   def draw(self):
     self.history.append(list(self.body.Position))
@@ -66,6 +69,17 @@ class plotting:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(xs, ys, zs, marker='o')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    x = np.linspace(-100, 100, 100)
+    y = np.linspace(-100, 100, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = self.Wall.Plane(X, Y)
+
+    ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.5)
     plt.show()
   
   def update(self, steps=100):
@@ -75,4 +89,5 @@ class plotting:
       if self.body.Position[2] <= 0:
         self.body.deflectionFromGround()
       self.history.append(list(self.body.Position))
+      self.Wall.VectorReflection()
     self.draw()
