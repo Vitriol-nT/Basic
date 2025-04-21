68803dca40c176238b46e2de8fcc963ef4362f31
Vitriol-nT <161344449+Vitriol-nT@users.noreply.github.com>
2025-04-21 17:19:42 +0900

    BasicPhysics.py

--- BasicSimulation/BasicPhysics.py
+++ BasicSimulation/BasicPhysics.py
@@ -6,7 +6,7 @@ import math
 import tkinter.messagebox as tkmsg
 
 Gravity = -9.8
-Wind = 1
+Wind = 0
 col = False
 
 class bodies:
@@ -43,19 +43,13 @@ class Wall:
     return z
 
   def VectorReflection(self):
-    global col
 
     Normal = (math.tan(self.rotation[0]), math.tan(self.rotation[1]), -1)
-    if self.body.Position[2] <= self.Plane(self.body.Position[0], self.body.Position[1]):
-      col = True
-
-    if col == True:
-      self.body.Position[2] = self.Plane(self.body.Position[0], self.body.Position[1])
-      #reflection
-      self.body.Velocity[0] = self.body.Velocity[0] + (-2 * Normal[0] * (self.body.Velocity[0] * Normal[0]))
-      self.body.Velocity[1] = self.body.Velocity[1] + (-2 * Normal[1] * (self.body.Velocity[1] * Normal[1]))
-      self.body.Velocity[2] = self.body.Velocity[2] + (-2 * Normal[2] * (self.body.Velocity[2] * Normal[2]))
-      col = False
+    self.body.Position[2] = self.Plane(self.body.Position[0], self.body.Position[1])
+    #reflection
+    self.body.Velocity[0] = self.body.Velocity[0] + (-2 * Normal[0] * (self.body.Velocity[0] * Normal[0]))
+    self.body.Velocity[1] = self.body.Velocity[1] + (-2 * Normal[1] * (self.body.Velocity[1] * Normal[1]))
+    self.body.Velocity[2] = self.body.Velocity[2] + (-2 * Normal[2] * (self.body.Velocity[2] * Normal[2]))
 
 class plotting:
   def __init__(self, body, Wall):
@@ -75,7 +69,7 @@ class plotting:
     ax.set_zlabel('Z')
 
     x = np.linspace(-100, 100, 100)
-    y = np.linspace(-100, 100, 100)
+    y = np.linspace(-10, 10, 100)
     X, Y = np.meshgrid(x, y)
     Z = self.Wall.Plane(X, Y)
 
@@ -86,8 +80,9 @@ class plotting:
     for _ in range(steps):
       self.body.Acceleration()
       self.body.move()
+      if self.body.Position[2] <= self.Wall.Plane(self.body.Position[0], self.body.Position[1]):
+        self.Wall.VectorReflection()
       if self.body.Position[2] <= 0:
         self.body.deflectionFromGround()
       self.history.append(list(self.body.Position))
-      self.Wall.VectorReflection()
     self.draw()
