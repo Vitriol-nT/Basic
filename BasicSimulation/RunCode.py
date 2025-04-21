cf15e09c69990436bfc8b651b7e30ff37c2fad59
Vitriol-nT <161344449+Vitriol-nT@users.noreply.github.com>
2025-04-21 17:20:01 +0900

    RunCode.py

--- BasicSimulation/RunCode.py
+++ BasicSimulation/RunCode.py
@@ -2,7 +2,7 @@ from BasicPhysics import bodies
 from BasicPhysics import plotting
 from BasicPhysics import Wall
 
-firstbody = bodies(Position=(0,0,100), Velocity=(0,2,0))
-thewall = Wall(firstbody, Position=(300,80,100), rotation=(0,1))
+firstbody = bodies(Position=(0,0,100), Velocity=(3,-1,3))
+thewall = Wall(firstbody, Position=(0,0,30), rotation=(-0.1,0.5))
 visualize = plotting(firstbody, thewall)
 visualize.update(35)
